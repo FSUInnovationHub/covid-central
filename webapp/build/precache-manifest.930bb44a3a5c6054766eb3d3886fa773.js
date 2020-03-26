47beabd969599971b89ba313a080ad11b8710db6
Orlando Kenny <kenny@cs.fsu.edu>
2020-03-26 15:50:21 -0400

    build

---
+++ webapp/build/precache-manifest.930bb44a3a5c6054766eb3d3886fa773.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a616599069d9c2ececb22e04455f72cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c95395d66150e39f3cd9",
+    "url": "/static/css/2.3d3aad01.chunk.css"
+  },
+  {
+    "revision": "816632af58b20825b750",
+    "url": "/static/css/main.faeaabfd.chunk.css"
+  },
+  {
+    "revision": "c95395d66150e39f3cd9",
+    "url": "/static/js/2.6d12aad8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6d12aad8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "816632af58b20825b750",
+    "url": "/static/js/main.60fefecb.chunk.js"
+  },
+  {
+    "revision": "08798f8fd45969ec2947",
+    "url": "/static/js/runtime-main.53c8a0e9.js"
+  }
+]);
