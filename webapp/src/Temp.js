2d34bf89df5caeeebfaab68ffe80f8116894e5e8
Orlando Kenny <kenny@cs.fsu.edu>
2020-03-26 15:29:06 -0400

    temp page

---
+++ webapp/src/Temp.js
@@ -0,0 +1,40 @@
+import React, {useState} from 'react';
+import './App.css';
+import { css } from "@emotion/core";
+import { Redirect } from 'react-router-dom';
+import 'react-spinning-wheel/dist/style.css';
+import ClockLoader from "react-spinners/ClockLoader";
+
+// Can be a string as well. Need to ensure each key-value pair ends with ;
+// allows for the clock to be centered
+const override = css`
+  display: block;
+  margin: 0 auto;
+  border-color: red;
+`;
+
+//This class will temporarily display 
+class Temp extends React.Component { 
+  render()
+  {  
+    return (   
+      <div> {/* DO NOT REMOVE THIS DIV COMPONENT*/}
+       <div className="tempCont">
+          <h1>Covid Central</h1>
+          <br></br>
+          <div className="clockWrapper">
+            <ClockLoader
+              css={override}
+              size={150}
+              color={"#FFFFFF"} 
+              />
+          </div>
+          <br></br>
+          <a className="see" href="https://github.com/FSUInnovationHub/covid-central" target="_blank">See what we're up to...</a>
+       </div>
+      </div>
+      )
+  }
+}
+export default Temp;
+
---
+++ webapp/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './App.css';
+import * as serviceWorker from './serviceWorker';
+import { Redirect, Switch, Route, BrowserRouter as Router, Link } from 'react-router-dom';
+import Temp from './Temp'
+
+/*using react router we set a constant equal to whatever component
+we would like to render*/
+const routing = (
+  <Router>
+    <Switch>
+      <Route exact path="/" component={Temp}/>
+    </Switch>
+  </Router>
+)
+
+/*the code below reads the path and renders component on a conditional basis. 
+i.e. /home throws two different components at different places...*/
+ReactDOM.render(routing, document.getElementById("root"));
+serviceWorker.unregister();
