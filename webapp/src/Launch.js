89b619b7b9dfa6d365b07361d1fc304fa62275e0
Orlando Kenny <kenny@cs.fsu.edu>
2020-03-27 14:30:56 -0400

    updated mobile ui

--- webapp/src/Launch.js
+++ webapp/src/Launch.js
@@ -4,6 +4,19 @@ import { css } from "@emotion/core";
 import { Redirect } from 'react-router-dom';
 import 'react-spinning-wheel/dist/style.css';
 import NumberFormat from 'react-number-format';
+import Select from 'react-select';
+
+const emotionsList = [
+  { value: 'anxious', label: 'anxious' },
+  { value: 'curious', label: 'curious' },
+];
+
+const sourcesList = [
+  { value: 'facts', label: 'facts' },
+  { value: 'stats', label: 'stats' },
+  { value: 'news', label: 'news' },
+  { value: 'commentary', label: 'commentary' },
+];
 
 //This page will display the current statistics from the COVID-19 Outbreak Specific to the USA
 class Stats extends React.Component { 
@@ -11,55 +24,64 @@ class Stats extends React.Component {
   constructor() {
     super();
     this.state = {
-      
+      emotion: null,
+      source: null,
     };
    
   }
   
+  handleChange = emotion => {
+    this.setState({ emotion });
+    console.log(`Option selected:`, emotion);
+  };
+
+  handleChange = source => {
+    this.setState({ source });
+    console.log(`Option selected:`, source);
+  };
   
 
 
 
   render()
   {  
+    const { selectedOption } = this.state;
     return (   
       <div> {/* DO NOT REMOVE THIS DIV COMPONENT*/}
         <div className="launchCont">
           <h1>Covid Central</h1>
           <h2 className="imFeeling">i'm feeling</h2>
-          <form action="https://covid-central.netlify.com/error" method="POST" > 
           <div className="dropdown">
-            <div align="center">
-              <select required name="feeling"> 
-                <option value="">select emotion</option>
-                <option value="curious">curious</option>
-                <option value="anxious">anxious</option>
-              </select>
-            </div>  
+            <Select
+              value={this.state.emotion}
+              onChange={this.handleChange}
+              options={emotionsList}
+            />
           </div>
 
           <h2 className="showMeThe">show me the</h2>
-       
+
           <div className="dropdown">
-            <div align="center">
-              <select required name="option"> 
-                <option value="">select source</option>
-                <option value="facts">facts</option>
-                <option value="stats">stats</option>
-                <option value="news">news</option>
-                <option value="commentary">commentary</option>
-              </select>
-            </div>  
+            <Select
+              value={this.state.source}
+              onChange={this.handleChange}
+              options={sourcesList}
+            />
           </div>
+          
+          <form action="https://covid-central.netlify.com/error" method="POST" > 
+          
+     
 
+          
+    
           
 
           
       
       
       
-      <footer className="submit"><button className="submitTxt" type="submit" value="Submit">submit</button></footer>
-        
+  <button className="submitTxt" type="submit" value="Submit">submit</button>
     </form>
           
     
