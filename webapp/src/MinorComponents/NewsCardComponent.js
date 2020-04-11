8fc6264f0122e17fa87edd016a50051bbd58d91d
huangshu91@gmail.com <huangshu@gmail.com>
2020-04-11 16:45:22 -0700

    render titles, clean up hardcoded string, clean up compiler warnings

--- webapp/src/MinorComponents/NewsCardComponent.js
+++ webapp/src/MinorComponents/NewsCardComponent.js
@@ -1,27 +1,31 @@
 import React from 'react';
-import './App.css';
+import '../App.css';
 import 'react-spinning-wheel/dist/style.css';
-
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
 
 class NewsCardComponent extends React.Component { 
 
   constructor(props) {
     super(props);
     this.state = {
-
+        articles: this.props.articles
     };
+
+    console.log(this.state.articles);
   }
 
   render()
   {  
-    return (   
-
-      )
+    return (
+<div>
+{
+    this.props.articles.map((article, i) => (
+      <div className="newsCont" key={i}>
+        <span className="cardTitle">{article.headline}</span>
+      </div>
+    ))
+}
+</div>
+    )
   }
 }
 export default NewsCardComponent;
---
+++ webapp/src/Shared/Enums.js
@@ -0,0 +1,14 @@
+export const CardResourceTypes = {
+    NEWS: 'NEWS',
+    FACTS: 'FACTS',
+    STATS: 'STATS',
+    RESOURCES: 'RESOURCES'
+}
+
+export const Emotions = {
+    ANXIETY: 'anxiety',
+    INNOVATION: 'innovation',
+    CURIOSITY: 'curiousity',
+    FACTUAL: 'factual',
+    INSPIRING: 'inspiring'
+}
