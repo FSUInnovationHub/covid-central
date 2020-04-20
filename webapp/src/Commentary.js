e3980d0798ac8063b0b5b60418f69187e0b6eb13
Orlando Kenny <kenny@cs.fsu.edu>
2020-04-20 19:12:28 -0400

    update

--- webapp/src/Commentary.js
+++ webapp/src/Commentary.js
@@ -74,12 +74,12 @@ class Commentary extends React.Component {
       {this.state.change === false && <TwitterTimelineEmbed
   sourceType="profile"
   screenName={this.state.username}
-  options={{height: "61vh"}}
+  options={{height: "75vh"}}
     />}
     {this.state.change === true && <TwitterTimelineEmbed
   sourceType="profile"
   screenName={this.state.username}
-  options={{height: "61vh"}}
+  options={{height: "75vh"}}
     />}
         
         <h1 className="tagCommentary">Service provided by the FSU Innovation Hub <br></br>
