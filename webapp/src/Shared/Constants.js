339c85c407be20c0ddb8940ea375246dd71fe722
Orlando Kenny <kenny@cs.fsu.edu>
2020-04-22 17:25:24 -0400

    better news endpoint

--- webapp/src/Shared/Constants.js
+++ webapp/src/Shared/Constants.js
@@ -1,3 +1,9 @@
 export const SheetsUrl = "https://spreadsheets.google.com/feeds/cells/187Oua2qj26uUd6lRYbuJoSVHw8-OGC9QvKvJfEewnkA/1/public/full?alt=json"
 
-export const NewsApi = "https://newsapi.org/v2/top-headlines?q=coronavirus&language=en&page=1&apiKey=e377370ade7c4b1eb951323b8740372f"
+export const NewsApi = "https://newsapi.org/v2/top-headlines?q=coronavirus&sortBy=datePublished&pageSize=100&language=en&apiKey=e377370ade7c4b1eb951323b8740372f"
+//https://newsapi.org/v2/everything?q=coronavirus&domains=wsj.com,%20cnn.com,%20abcnews.go.com,%20apnews.com,%20axios.com,%20bloomberg.com,%20businessinsider.com,%20cbsnews.com,%20cnbc.com,%20foxnews.com,%20news.google.com,%20nbcnews.com,%20nextbigfuture.com,%20reuters.com,%20techcrunch.com,%20techradar.com,%20washingtontimes.com,%20time.com,%20usatoday.com/news,%20news.vice.com,%20wired.com&sortBy=datePublished&pageSize=100&page=1&apiKey=e377370ade7c4b1eb951323b8740372f"
+//https://newsapi.org/v2/everything?q=coronavirus&domains=wsj.com,%20cnn.com,%20abcnews.go.com,%20apnews.com,%20axios.com,%20bloomberg.com,%20businessinsider.com,%20cbsnews.com,%20cnbc.com,%20foxnews.com,%20news.google.com,%20nbcnews.com,%20nextbigfuture.com,%20reuters.com,%20techcrunch.com,%20techradar.com,%20washingtontimes.com,%20time.com,%20usatoday.com/news,%20news.vice.com,%20wired.com&sortBy=popular&page=2&apiKey=e377370ade7c4b1eb951323b8740372f"
+
+//https://newsapi.org/v2/top-headlines?q=coronavirus&language=en&page=1&apiKey=e377370ade7c4b1eb951323b8740372f"
+
+//https://newsapi.org/v2/everything?q=coronavirus&domains=wsj.com, cnn.com, abcnews.go.com, apnews.com, axios.com, bloomberg.com, businessinsider.com, cbsnews.com, cnbc.com, foxnews.com, news.google.com, nbcnews.com, nextbigfuture.com, reuters.com, techcrunch.com, techradar.com, washingtontimes.com, time.com, usatoday.com/news, news.vice.com, wired.com&sortBy=datePublished&page=1&apiKey=e377370ade7c4b1eb951323b8740372f
