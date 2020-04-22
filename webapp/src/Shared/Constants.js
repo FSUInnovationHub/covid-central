074b4296ec9df486b00c2cbf058ed689750f5f04
Orlando Kenny <kenny@cs.fsu.edu>
2020-04-22 09:11:27 -0400

    better news endpoint

--- webapp/src/Shared/Constants.js
+++ webapp/src/Shared/Constants.js
@@ -1,3 +1,3 @@
 export const SheetsUrl = "https://spreadsheets.google.com/feeds/cells/187Oua2qj26uUd6lRYbuJoSVHw8-OGC9QvKvJfEewnkA/1/public/full?alt=json"
 
-export const NewsApi = "https://newsapi.org/v2/everything?q=coronavirus&from=2020-04-19&sortBy=popularity&page=1&apiKey=e377370ade7c4b1eb951323b8740372f"
+export const NewsApi = "https://newsapi.org/v2/top-headlines?q=coronavirus&language=en&page=1&apiKey=e377370ade7c4b1eb951323b8740372f"
