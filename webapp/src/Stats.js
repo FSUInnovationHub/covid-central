977d110c69205fe3ee50e2b52e325d166ccce2c4
Orlando Kenny <kenny@cs.fsu.edu>
2020-08-25 01:45:41 -0400

    Update Stats.js

--- webapp/src/Stats.js
+++ webapp/src/Stats.js
@@ -435,7 +435,7 @@ class Stats extends React.Component {
               {charts[1]}
             </div>
 
-            <div className="graphCont" id="container" style={{marginTop: "20px"}}>
+      {/*<div className="graphCont" id="container" style={{marginTop: "20px"}}>
               <br></br>
               <Select className="selectFacts"
                 placeholder={this.state.stateGraph}
@@ -445,7 +445,7 @@ class Stats extends React.Component {
               <div className="graphCont">
               {stateCharts[this.state.stateIndex]}
               </div>
-            </div>
+            </div>*/}
           
             {/*TOP TEN COUNTRIES WIDGET*/}
             <div className="topTenCont" style={{marginTop: "20px"}}>
